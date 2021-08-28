507cdf6b7fb5d44c911491865b5e478f4bd55fca
Natalia <59486546+talcia@users.noreply.github.com>
2021-08-28 22:52:31 +0200

    update starting page

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,39 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Units from '../UI/Unit/Units';
-
-import classes from './StartingPageContent.module.css';
+import LoggedStarting from './LoggedStarting';
+import UnloggedStarting from './UnloggedStarting';
 
 const StartingPageContent = () => {
-	const [units, setUnits] = useState([]);
-	const token = useSelector((state) => state.auth.token);
+	const isAuth = useSelector((state) => state.auth.isAuthenticated);
 
-	const url = `${process.env.REACT_APP_BACKENDURL}/units`;
-
-	useEffect(() => {
-		fetch(url, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${token}`,
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => setUnits(data.units));
-	}, [url, token]);
-
-	return (
-		<>
-			<div className={classes.title}>
-				<h2>Check the most popular units</h2>
-				<p>
-					get max points of each and become{' '}
-					<span>language master</span>
-				</p>
-			</div>
-			<Units units={units} />
-		</>
-	);
+	return <>{isAuth ? <LoggedStarting /> : <UnloggedStarting />}</>;
 };
 
 export default StartingPageContent;
