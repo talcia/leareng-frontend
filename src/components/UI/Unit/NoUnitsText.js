dc9a28fbf584bcbd8706a45e47f80d12e632611b
Natalia <59486546+talcia@users.noreply.github.com>
2021-08-29 17:49:18 +0200

    add noUnitsText component

---
+++ src/components/UI/Unit/NoUnitsText.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import classes from './NoUnitsText.module.css';
+
+const NoUnitsText = ({ text, linkText, link }) => {
+	return (
+		<div className={classes.noUnit}>
+			<p>
+				{text} <Link to={link}>{linkText}</Link>
+			</p>
+		</div>
+	);
+};
+
+export default NoUnitsText;
