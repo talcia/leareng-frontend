68aa58545ca2c8e48738003e8f090352b9cd0c2b
Natalia <59486546+talcia@users.noreply.github.com>
2021-08-30 23:11:05 +0200

    update loggedStarting and unloggedStarting page

--- src/components/StartingPage/UnloggedStarting.js
+++ src/components/StartingPage/UnloggedStarting.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TitleText from '../UI/TitleText';
 
 import classes from './UnloggedStarting.module.css';
 
 const UnloggedStarting = () => {
 	return (
 		<>
-			<div className={classes.title}>
-				<h2>Check the most popular units or create your own</h2>
-				<p>
-					get max points of each unit and become{' '}
-					<span>language master</span>
-				</p>
-			</div>
-			<div className={classes.desc}>
-				lorem ipsum sad a as asda ds lorem ipsum sad a as asda ds lorem
-				ipsum sad a as asda ds lorem ipsum sad a as asda ds
-			</div>
+			<TitleText
+				title="Check the most popular units or create your own"
+				text={
+					<>
+						get max points of each unit and become
+						<span>language master</span>
+					</>
+				}
+			/>
 			<div className={classes.action}>
 				<p>
 					But first you need to <Link to="/auth/login">login</Link> if
