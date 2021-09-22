ac59ba90f7a6bb444da9430d2bf4eccc499ac572
Natalia <59486546+talcia@users.noreply.github.com>
2021-09-22 21:26:06 +0200

    update Input component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -4,7 +4,7 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
 	return (
-		<div className={classes.input}>
+		<div className={classes.input} style={props.style}>
 			<input
 				type={props.type}
 				id={props.id}
