2bffbe0730a90cd80a048d0a189646884feee649
Natalia <59486546+talcia@users.noreply.github.com>
2021-09-25 23:22:29 +0200

    update colors styles

---
+++ src/constans/colors.js
@@ -0,0 +1,19 @@
+const lightTheme = {
+	'--main-background': '#f8f8f8',
+	'--light-blue': '#eaf6fd',
+	'--orange': '#f16d21',
+	'--dark-blue': '#143047',
+	'--grey': '#77777a',
+	'--light-grey': '#dfdfe9',
+};
+
+const darkTheme = {
+	'--main-background': '#2c2c2c',
+	'--light-blue': '#142735',
+	'--orange': '#f16d21',
+	'--dark-blue': '#e6edf2',
+	'--grey': '#77777a',
+	'--light-grey': '#dfdfe9',
+};
+
+export { lightTheme, darkTheme };
