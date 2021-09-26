10753cf7e96618ac844dc45e9369d97b7d87eb02
Natalia <59486546+talcia@users.noreply.github.com>
2021-09-26 20:02:22 +0200

    add toggle button to settings page

--- src/components/Account/Settings.js
+++ src/components/Account/Settings.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { changeToDarkMode, changeToLightMode } from '../../store/theme-actions';
+import ToggleButton from 'react-toggle-button';
 
 import classes from './Settings.module.css';
 
@@ -24,14 +25,31 @@ const Settings = () => {
 		<div className={classes.settings}>
 			<h2>Settings</h2>
 			<div className={classes.actions}>
-				<div>
-					<p>
-						Dark Mode{' '}
-						<span onClick={onDarkModeHandler}>
-							{darkModeIsOn ? 'off' : 'on'}
-						</span>
-					</p>
-				</div>
+				<p>{darkModeIsOn ? 'Light mode' : 'Dark Mode'}</p>
+				<ToggleButton
+					className={classes.toggleButton}
+					inactiveLabel={''}
+					activeLabel={''}
+					colors={{
+						activeThumb: {
+							base: '#f16d21',
+						},
+						inactiveThumb: {
+							base: '#f16d21',
+						},
+						active: {
+							base: '#77777a',
+							hover: '#aaaaaf',
+						},
+						inactive: {
+							base: '#dfdfe9',
+							hover: '#c8c8d3',
+						},
+					}}
+					thumbAnimateRange={[0, 34]}
+					value={darkModeIsOn}
+					onToggle={onDarkModeHandler}
+				/>
 			</div>
 		</div>
 	);
