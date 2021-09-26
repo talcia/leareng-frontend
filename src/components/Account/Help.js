6b01759fff643ae4bca203b971bb930cf609086f
Natalia <59486546+talcia@users.noreply.github.com>
2021-09-26 20:02:05 +0200

    add help component

---
+++ src/components/Account/Help.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Help.module.css';
+
+const Help = () => {
+	return (
+		<div className={classes.help}>
+			<h2>Support and Helpdesk</h2>
+			<p>Nothing here yet</p>
+		</div>
+	);
+};
+
+export default Help;
