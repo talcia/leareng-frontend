06e9bfe6e9f9a6dd696427c2c824e0c1de53180f
Natalia <59486546+talcia@users.noreply.github.com>
2021-10-07 16:03:29 +0200

    add answersTable component

---
+++ src/components/Game/AnswersTable.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import classes from './AnswersTable.module.css';
+
+const AnswersTable = ({ ratedAnswers }) => {
+	return (
+		<div className={classes.translation}>
+			<table>
+				<tbody className={classes.title}>
+					<tr>
+						<td>Question</td>
+						<td>Your translation</td>
+						<td>Correct translation</td>
+					</tr>
+				</tbody>
+				<tbody className={classes.words}>
+					{ratedAnswers.map((item) => (
+						<tr key={item._id}>
+							<td>{item.word.join(', ')}</td>
+							<td
+								className={
+									item.correct ? classes.good : classes.bad
+								}
+							>
+								{item.answer}
+							</td>
+							<td className={!item.correct ? classes.good : ''}>
+								{item.translation.join(', ')}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default AnswersTable;
